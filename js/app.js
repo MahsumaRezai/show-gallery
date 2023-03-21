99c86e81cf640071726174587e1468bee99ac161
Masoumeh Rezai <you@example.com>
2023-03-22 00:31:54 +0430

    add function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+
 let image = new Array("./imag/image_one.jpg", "./imag/image_two.jpg", "./imag/image_four.jpg", "./imag/three.jpeg", "./imag/image_four.jpg");
+let i = 0;
+let time;
+function show() {
+    if (i > 4) {
+        i = 0;
+    }
+    document.getElementById("slider").innerHTML = "<img src = '" + image[i] + "'>";
+    i++;
+    time = window.setTimeout(show, 2000)
+
+}
+function puse() {
+    window.clearTimeout(time)
+}
+function start() {
+    show()
+}
+
+
